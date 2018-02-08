ab9ad618a006d4828387546f6449d533e730854e
Subash <sesubash@gmail.com>
2018-02-09 02:31:37 +0530

    updated init logic

--- src/placeholder-shown-polyfill.js
+++ src/placeholder-shown-polyfill.js
@@ -2,6 +2,7 @@
 
   function init() {
     document.querySelectorAll('[placeholder]').forEach(el => {
+      placeholderPolyfill();
       el.addEventListener('change', placeholderPolyfill);
       el.addEventListener('keyup', placeholderPolyfill);
     });
