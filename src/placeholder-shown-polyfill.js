050c69d504f0f7c984c4b18c3bea464fce3df410
Subash <sesubash@gmail.com>
2018-02-09 02:04:40 +0530

    core logic added

---
+++ src/placeholder-shown-polyfill.js
@@ -0,0 +1,21 @@
+(function(){
+
+  function init() {
+    document.querySelectorAll('[placeholder]').forEach(el => {
+      el.addEventListener('change', placeholderPolyfill);
+      el.addEventListener('keyup', placeholderPolyfill);
+    });
+  }
+
+  function placeholderPolyfill() {
+    this.classList[this.value ? 'remove' : 'add']('placeholder-shown');
+  }
+
+  if (/^[c|i]|d$/.test(document.readyState || "")) {
+  	init();
+  } else {
+  	document.addEventListener("DOMContentLoaded", init);
+  }
+
+
+})();
